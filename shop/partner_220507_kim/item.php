ac4f9afc3475f3f5d65958865d8dd79bca0c95c7
root <root@vultr.guest>
2022-07-20 11:43:04 +0900

    파일내용정리-partner_220507_kim

--- shop/partner_220507_kim/item.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once('./_common.php');
-
-goto_url(G5_SHOP_URL.'/item.php?it_id='.$it_id);
-
-?>
--- shop/partner_220507_kim/itemqalist.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once('./_common.php');
-
-goto_url(G5_SHOP_URL.'/itemqalist.php?sfl='.urlencode($sfl).'&amp;stx='.urlencode($stx));
-
-?>
