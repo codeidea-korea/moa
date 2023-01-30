56e7d435a591e42356d3b2f52597829b3c6679a8
DESKTOP-1R6NKID\fuser <jyk586@gmail.com>
2023-01-30 18:22:46 +0900

    이미지 첨부파일 초기화 부분 수정

--- shop/partner/skin/Class-Board/write/basic/write.skin.php
+++ shop/partner/skin/Class-Board/write/basic/write.skin.php
@@ -959,8 +959,9 @@ $('.fileContainer input[type="file"]').each(function(index) {
 					alert('가로/세로는 같은 사이즈를 권장합니다.');
 					console.log(img.width);
 					console.log(img.height);
-//					upload.files[0] = null;
-//					return;
+                    $('input[type="file"]').val("");
+					upload.files[0] = null;
+					return;
 				} 
 				const key = new Date().getTime();
 				last.before('<li>' + imgtag + '<span class="del" data-key="'+(key)+'"></span></li>');
@@ -968,7 +969,7 @@ $('.fileContainer input[type="file"]').each(function(index) {
 				$('#upload-01' + uploadKey).attr('data-key', key);
 				uploadKey = uploadKey + 1;
 				$('.upload-btn').attr('for', 'upload-01'+uploadKey);
-				$('.upload-empty').attr('for', 'upload-01'+uploadKey);
+				$('.upload-empty').attr('for', 'upload-021'+uploadKey);
 				
 				$('#upload-del-01'+uploadKey).val('1');
 				deleteImageAction('.del');
